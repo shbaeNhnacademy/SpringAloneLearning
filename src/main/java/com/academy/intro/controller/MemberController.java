60b30f3d4dc25cd11e64003d2f3d2a97249fab38
배수한 <qotngks152@naver.com>
2022-10-08 00:45:12 +0900

    v1.0

--- src/main/java/com/academy/intro/controller/MemberController.java
+++ src/main/java/com/academy/intro/controller/MemberController.java
@@ -4,6 +4,10 @@ import com.academy.intro.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
+/**
+ * Test 필요
+ */
+
 @Controller
 public class MemberController {
     public final MemberService memberService;
